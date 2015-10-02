603395cc1ab8ecefd9bf71a7ffb8c9ec60affcb2
Christian <c.pradelli@sig2k.com>
2015-10-02 19:57:29 -0300

    start work on glyphicon preview

---
+++ glyphicon-res/Unit2.dfm
@@ -0,0 +1,44 @@
+object Form2: TForm2
+  Left = 0
+  Top = 0
+  Caption = 'Form2'
+  ClientHeight = 557
+  ClientWidth = 912
+  Color = clBtnFace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -11
+  Font.Name = 'Tahoma'
+  Font.Style = []
+  OldCreateOrder = False
+  PixelsPerInch = 96
+  TextHeight = 13
+  object Memo1: TMemo
+    Left = 24
+    Top = 51
+    Width = 765
+    Height = 477
+    Font.Charset = DEFAULT_CHARSET
+    Font.Color = clWindowText
+    Font.Height = -27
+    Font.Name = 'GLYPHICONS Halflings'
+    Font.Style = []
+    ParentFont = False
+    TabOrder = 0
+  end
+  object Button1: TButton
+    Left = 24
+    Top = 20
+    Width = 109
+    Height = 25
+    Caption = 'Process'
+    Font.Charset = DEFAULT_CHARSET
+    Font.Color = clWindowText
+    Font.Height = -11
+    Font.Name = 'Tahoma'
+    Font.Style = []
+    ParentFont = False
+    TabOrder = 1
+    OnClick = Button1Click
+  end
+end
---
+++ glyphicon-res/Unit2.pas
@@ -0,0 +1,66 @@
+unit Unit2;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls;
+
+type
+  TForm2 = class(TForm)
+    Memo1: TMemo;
+    Button1: TButton;
+    procedure Button1Click(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form2: TForm2;
+
+implementation
+
+{$R *.dfm}
+
+uses System.StrUtils;
+
+procedure TForm2.Button1Click(Sender: TObject);
+var
+  css, res: TStringList;
+  i: integer;
+  name, valu: string;
+  intv: integer;
+  sample: string;
+begin
+  sample := '';
+
+  css := TStringList.Create;
+  res := TStringList.Create;
+  try
+    css.LoadFromFile('..\demo\bin\wwwroot\iwbs\bootstrap-3.3.5\css\bootstrap.css');
+    for i := 0 to css.Count-1 do
+      if AnsiStartsStr('.glyphicon-',css[i]) and AnsiContainsStr(css[i+1],'content:') then begin
+        name := copy(css[i], 2, Pos(':', css[i])-2);
+        valu := Copy(css[i+1], Pos('\', css[i+1])+1, MaxInt);
+        valu := Copy(valu, 1, Pos('"', valu)-1);
+        try
+          intv := StrToInt('$'+valu);
+          res.Add(name+'='+IntToStr(intv));
+
+          sample := sample + Char(intv);
+        except
+          ShowMessage('error in line '+IntToStr(i)+' '+name+' '+valu);
+        end;
+      end;
+
+    res.SaveToFile('..\source\glyphicon-res.txt');
+    Memo1.Text := sample;
+  finally
+    res.Free;
+    css.Free;
+  end;
+end;
+
+end.
